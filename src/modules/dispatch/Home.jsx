0af218e7995d3bac305564379dc384de4fdb594a
nikita <devstarp@example.com>
2022-03-19 12:19:21 +0700

    Add: search and fetch student feature in search page

--- src/modules/dispatch/Home.jsx
+++ src/modules/dispatch/Home.jsx
@@ -10,22 +10,18 @@ const Home = ({props}) => {
 
     const dispatch = useDispatch();
     const students = useSelector((state) => state.students.students);
+    const [student, setStudent] = useState({});
     const [filterText, setFilterText] = useState('');
     const filterStudents = students.filter(
         (item) => (item.firstName &&
                     item.firstName
-                        .toLowerCase()
-                        .includes(filterText.toLowerCase()))||
-                        (item.lastName &&
-                            item.lastName
-                                .toLowerCase()
-                                .includes(filterText.toLowerCase()))||
-                        (item.barcodeNumber &&
-                            item.barcodeNumber
-                                .toLowerCase()
-                                .includes(filterText.toLowerCase()))
-                
-                        
+                        .toLowerCase() == (filterText.toLowerCase()))||
+                (item.lastName &&
+                    item.lastName
+                        .toLowerCase() == (filterText.toLowerCase()))||
+                (item.barcodeNumber &&
+                    item.barcodeNumber
+                        .toLowerCase() == (filterText.toLowerCase()))                
     );
 
     let columns = !students.length
@@ -35,40 +31,94 @@ const Home = ({props}) => {
               selector: (row) => row[c],
               sortable: true
           }));
+
+
+    const getStudent = () =>{
+        var filterStudents = students.filter(
+            (item) => (item.firstName &&
+                        item.firstName
+                            .toLowerCase()==(filterText.toLowerCase()))||
+                            (item.lastName &&
+                                item.lastName
+                                    .toLowerCase()==(filterText.toLowerCase()))||
+                            (item.barcodeNumber &&
+                                item.barcodeNumber
+                                    .toLowerCase()==(filterText.toLowerCase())));
+
+        if(filterStudents.length>0)
+            setStudent(filterStudents[0]);
+        else
+            setStudent({});
+    }
     return (
         <Main>
             <div className="container-fluid">
                 <h2 className="text-center display-4">Search</h2>
-                <div className="row">
+                <div className="row" style={{marginBottom: 30}}>
                     <div className="col-md-8 offset-md-2">
                         <div className="input-group">
-                        <input type="search" onKeyPress={(e) => {if (e.key === 'Enter') setFilterText(e.target.value)} }  className="form-control form-control-lg" placeholder="Type your keywords here"></input>
+                        <input type="search" onChange={(e) => setFilterText(e.target.value)} onKeyPress={(e) => {if (e.key === 'Enter') getStudent()} }  className="form-control form-control-lg" placeholder="Type your keywords here"></input>
                             <div className="input-group-append">
-                                <Button>
+                                <Button onClick={(e) => getStudent()}>
                                     <i className="fa fa-search"></i>
                                 </Button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <DataTable
-                        striped
-                        subHeader
-                        subHeaderAlign="right"
-                        highlightOnHover
-                        title="Students"
-                        fixedHeader
-                        fixedHeaderScrollHeight="300px"
-                        pagination
-                        selectableRows
-                        columns={columns}
-                        data={filterStudents}
-                    />
-                </div>
-
+                {
+                    // student.firstName ?
+                    <div className="row">
+                        <div className="col-4"></div>
+                        <div className="col-4">
+                            <div className="card card-widget widget-user">
+                            <div className="widget-user-header bg-primary">
+                                <h3 className="widget-user-username">{student.firstName}</h3>
+                                <h5 className="widget-user-desc">{student.lastName}</h5>
+                            </div>
+                            <div className="widget-user-image">
+                                <img className="img-circle elevation-2" src={'/img/default-profile.png'} alt="User Avatar"></img>
+                            </div>
+                            <div className="card-footer">
+                                <div className="row">
+                                    <div className="col-sm-4 border-right">
+                                        <div className="description-block">
+                                        <h5 className="description-header">{student.classroom}</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-4 border-right">
+                                        <div className="description-block">
+                                        <h5 className="description-header">{student.homeroomTeacher}</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-4 border-right">
+                                        <div className="description-block">
+                                        <h5 className="description-header">{student.barcodeNumber}</h5> 
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row center">
+                                    <div className="col-sm-4 border-right">
+                                    </div>
+                                    <div className="col-sm-4 border-right">
+                                        <Button onClick={(e) => getStudent()}>
+                                            <i className="fa fa-user"></i> Dispatch
+                                        </Button>
+                                    </div>
+                                    <div className="col-sm-4 border-right">
+                                    </div>
+                                </div>
+                            </div>
+                            </div>    
+                        </div>
+                        <div className="col-4"></div>
+                    </div>
+                    // : 
+                    //     <p>
+                            
+                    //     </p>
+                }
             </div>
-
         </Main>
     );
 };
